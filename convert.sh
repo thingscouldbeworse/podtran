f5852901417e422643f965887931a5885327067f
Kirk Hardy <megal33tdude@gmail.com>
2019-04-08 18:26:58 -0400

    full script fix and convert fix

--- convert.sh
+++ convert.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 for PATHNAME in episodes/*.mp3
 do
-  ffmpeg -i "$FILENAME" thumb.jpeg
+  ffmpeg -y -i "$PATHNAME" thumb.jpeg
   FILENAME="${PATHNAME##*/}"
   ffmpeg -loop 1 -i thumb.jpeg -i "$PATHNAME" -c:a copy -c:v libx264 -shortest "episodes_convert/${FILENAME%.mp3}.mp4"
 done
--- download_convert_upload.sh
+++ download_convert_upload.sh
@@ -4,8 +4,7 @@ EPISODES_OUT='episodes_convert/'
 POD_NAME=$1 
 SECRETS=$2
 
-cd $EPISODES_DIR && soundscrape $POD_NAME
-
-./convert.sh
-
-python3 upload_vids.py "$2"
+cd "$EPISODES_DIR" && soundscrape "$POD_NAME"
+cd ../
+sh ./convert.sh
+python3 upload_vids.py --secrets "$2"
