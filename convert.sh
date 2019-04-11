df0ae1b0eb6c9de961950d5a5b368ff8982d7813
Kirk Hardy <megal33tdude@gmail.com>
2019-04-10 23:49:49 -0400

    skip ffmpeg if output exists

--- convert.sh
+++ convert.sh
@@ -3,6 +3,6 @@ for PATHNAME in episodes/*.mp3
 do
   ffmpeg -y -i "$PATHNAME" thumb.jpeg
   FILENAME="${PATHNAME##*/}"
-  ffmpeg -loop 1 -i thumb.jpeg -i "$PATHNAME" -c:a copy -c:v libx264 -shortest "episodes_convert/${FILENAME%.mp3}.mp4"
+  ffmpeg -n -loop 1 -i thumb.jpeg -i "$PATHNAME" -c:a copy -c:v libx264 -shortest "episodes_convert/${FILENAME%.mp3}.mp4"
 done
 
